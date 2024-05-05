efa9c116bf831de0bbc41e5ec8e379401819bb65
Kumarabh <abhishek@gmail.com>
2024-05-05 12:19:50 +0530

    feat: hibernate-learning more file added

--- src/main/java/com/object_states/application/App.java
+++ src/main/java/com/object_states/application/App.java
@@ -1,5 +1,10 @@
 package com.object_states.application;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SessionFactory factory = new Configuration().configure().buildSessionFactory();
+    	Session session = factory.openSession();
+    	
+    	Transaction tsx = session.beginTransaction();
+    	Customer c1 = new Customer("John Doe");
+    	session.save(c1);
+    	tsx.commit();
+
+    	Customer c2 = new Customer();
+    	c2.setCustomerName("James Smith"); // Customer c2 won't be saved. Transaction is already commited.
+
+    	session.save(c2);
+    	factory.close();
+    	
     }
 }
---
+++ src/main/java/com/object_states/application/Customer.java
@@ -0,0 +1,51 @@
+package com.object_states.application;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int customerId;
+	
+	@Column
+	private String customerName;
+
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(String customerName) {
+		super();
+//		this.customerId = customerId;
+		this.customerName = customerName;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	
+	
+}
